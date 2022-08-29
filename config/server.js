3d1f5411a62982c80def18a720f50ae9cc7053b1
Zhenwei Wang <jack230230@gmail.com>
2022-08-29 19:23:52 +0800

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://' + env('ENV_DOMAIN'),
   app: {
     keys: env.array('APP_KEYS'),
-    url: 'http://' + env('ENV_DOMAIN'),
   },
 });
